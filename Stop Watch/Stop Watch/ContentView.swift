c69d7da48f0cd9feb0311a3309138f55114f01f5
Harsh Patel <harshpatel7.list@gmail.com>
2020-11-09 21:51:39 -0500

    init

---
+++ Stop Watch/Stop Watch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stop Watch
+//
+//  Created by Harsh Patel on 11/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stop Watch/Stop Watch/Stop_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_WatchApp.swift
+//  Stop Watch
+//
+//  Created by Harsh Patel on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Stop_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
